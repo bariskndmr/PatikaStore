a95d0bddbaacff57f2b276c7fffe0599bcaac66b
Barış Kandemir <baris-kndmr@hotmail.com>
2021-11-05 13:45:11 +0300

    json data added

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import SearchInput from './src/components/input';
 import ProductCard from './src/components/Card';
+import Products_Data from './src/products_data.json';
 
 export default App = () => {
+  const productRender = ({item}) => <ProductCard products={item}/>
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>PATIKASTORE</Text>
       <SearchInput />
+      <FlatList 
+      data={Products_Data}
+      renderItem={productRender}
+      numColumns= '2'
+      />
     </SafeAreaView>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './Card.style';
 
-export default Card = () => {
+export default Card = ({products}) => {
+    console.log(products);
     return (
         <View>
-            <Image style={styles.image}/>
+            <Image style={styles.image} source={{uri : products.imgUrl}}/>
             <View style={styles.textsContainer}>
-                <Text style={styles.title}></Text>            
-                <Text style={styles.price}></Text>
-                <Text style={styles.stock}></Text>
+                <Text style={styles.title}>{products.title}</Text>            
+                <Text style={styles.price}>{products.price}</Text>
+                <Text style={styles.stock}>{products.inStock}</Text>
             </View>
         </View>
     );
